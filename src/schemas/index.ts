734ad1cd3a687cfdb57d342bd71b3445be10460d
Tasnim Hasan <ummetasnim@iut-dhaka.edu>
2024-03-20 03:59:57 +0600

    schema updated

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -11,11 +11,14 @@ export const SignupSchema = z.object({
   email: z.string().email(),
   password: z
     .string()
-    .min(6, {
-      message: "Password must be at least 6 characters long!",
+    .min(8, {
+      message: "Password must be at least 8 characters long!",
     })
     .max(50),
   name: z.string().min(1, {
     message: "Name is required",
+  }),
+  username: z.string().min(1, {
+    message: "Username is required",
   }),
 });
