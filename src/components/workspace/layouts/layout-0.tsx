0fa453d33cb699e2e905805eb3dfd520083bf912
ShojebBhuiyan <shojeb.bhuiyan@gmail.com>
2024-04-07 22:52:07 +0600

    Pregeneration fixed

--- src/components/workspace/layouts/layout-0.tsx
+++ src/components/workspace/layouts/layout-0.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { getInitialRenderedScene } from "@/actions/render/get-initial-rendered-scene";
 import {
@@ -11,8 +11,8 @@ import { useEffect, useState } from "react";
 import { Panel } from "../panel";
 
 export function Layout0({ projectId, page, nbPanels }: LayoutProps) {
-  
-  const [scenes, setScenes] = useState<RenderedScene[] | undefined>([]);
+
+  const [scenes, setScenes] = useState<RenderedScene[]>([]);
 
   useEffect(() => {
     getInitialRenderedScene(projectId).then((data) => {
@@ -20,7 +20,7 @@ export function Layout0({ projectId, page, nbPanels }: LayoutProps) {
         const updatedData = data.map(item => ({
           ...item,
           status: item.status as RenderedSceneStatus,
-          segments: [], 
+          segments: [],
         }));
         setScenes(updatedData);
       }
@@ -29,58 +29,62 @@ export function Layout0({ projectId, page, nbPanels }: LayoutProps) {
 
   return (
     <ResizablePanelGroup direction="vertical" className="border">
-      <ResizablePanel defaultSize={50}>
-        <ResizablePanelGroup direction="horizontal">
+      {scenes.length > 0 && (
+        <>
           <ResizablePanel defaultSize={50}>
-            <Panel
-              savedUrl={scenes?.at(0)?.assetUrl}
-              page={page}
-              nbPanels={nbPanels}
-              panel={0}
-              width={1024}
-              height={1024}
-            />
+            <ResizablePanelGroup direction="horizontal">
+              <ResizablePanel defaultSize={50}>
+                <Panel
+                  savedUrl={scenes?.at(0)?.assetUrl}
+                  page={page}
+                  nbPanels={nbPanels}
+                  panel={0}
+                  width={1024}
+                  height={1024}
+                />
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel defaultSize={50}>
+                <Panel
+                  savedUrl={scenes?.at(1)?.assetUrl}
+                  page={page}
+                  nbPanels={nbPanels}
+                  panel={1}
+                  width={1024}
+                  height={1024}
+                />
+              </ResizablePanel>
+            </ResizablePanelGroup>
           </ResizablePanel>
           <ResizableHandle />
           <ResizablePanel defaultSize={50}>
-            <Panel
-              savedUrl={scenes?.at(1)?.assetUrl}
-              page={page}
-              nbPanels={nbPanels}
-              panel={1}
-              width={1024}
-              height={1024}
-            />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </ResizablePanel>
-      <ResizableHandle />
-      <ResizablePanel defaultSize={50}>
-        <ResizablePanelGroup direction="horizontal">
-          <ResizablePanel defaultSize={50}>
-            <Panel
-              savedUrl={scenes?.at(2)?.assetUrl}
-              page={page}
-              nbPanels={nbPanels}
-              panel={2}
-              width={1024}
-              height={1024}
-            />
+            <ResizablePanelGroup direction="horizontal">
+              <ResizablePanel defaultSize={50}>
+                <Panel
+                  savedUrl={scenes?.at(2)?.assetUrl}
+                  page={page}
+                  nbPanels={nbPanels}
+                  panel={2}
+                  width={1024}
+                  height={1024}
+                />
+              </ResizablePanel>
+              <ResizableHandle />
+              <ResizablePanel defaultSize={50}>
+                <Panel
+                  savedUrl={scenes?.at(3)?.assetUrl}
+                  page={page}
+                  nbPanels={nbPanels}
+                  panel={3}
+                  width={1024}
+                  height={1024}
+                />
+              </ResizablePanel>
+            </ResizablePanelGroup>
           </ResizablePanel>
           <ResizableHandle />
-          <ResizablePanel defaultSize={50}>
-            <Panel
-              savedUrl={scenes?.at(3)?.assetUrl}
-              page={page}
-              nbPanels={nbPanels}
-              panel={3}
-              width={1024}
-              height={1024}
-            />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </ResizablePanel>
-      <ResizableHandle />
+        </>
+      )}
     </ResizablePanelGroup>
   );
 }
